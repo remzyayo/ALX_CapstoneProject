44e90b21820ef9f7c8d52583604e5b532b5ec024
Francis <oluremifrancisaya@gmailcom>
2025-10-10 08:21:56 +0100

    Implemented filtering, search, and pagination for expenses

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_filters',
     'expenses',
     'users',
+    'django_filters',
 ]
 
 AUTH_USER_MODEL = 'users.User'
@@ -136,4 +137,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
+    'DEFAULT_FILTER_BACKENDS':[
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
--- expenses/views.py
+++ expenses/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
-
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Expense
 from .serializer import ExpenseSerializer
+from django.db.models import Sum
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['category', 'date']
+    search_fields = ['description']
+    ordering_fields = ['amount', 'date']
 
     def get_queryset(self):
-        # Return only expenses belonging to the logged-in user
         return Expense.objects.filter(user=self.request.user)
-
+    
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        return super().perform_create(serializer)
 
+    @action(detail=False, methods=['get'])
+    def summary(self, request):
+        user = request.user
+        expenses = Expense.objects.filter(user=user)
+        total_spent = expenses.aggregate(Sum('amount'))['amount__sum'] or 0
+        total_budget = expenses.aggregate(Sum('budget'))['budget__sum'] or 0
+        remaining = total_budget - total_spent
+
+        return Response({
+            'total_spent': total_spent,
+            'total_budget': total_budget,
+            'remaining_balance': remaining
+        })
 
