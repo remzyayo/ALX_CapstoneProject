1eb647de4274ab66bd5911b45f22375153c4987e
Francis <oluremifrancisaya@gmailcom>
2025-10-17 07:35:41 +0100

    Fixed django_filters duplication issue

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwtexpenses',
+
     'expenses',
     'users',
 ]
