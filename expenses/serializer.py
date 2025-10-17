6f758ec842c0c3120661fd4df76c30e47214cd2c
Francis <oluremifrancisaya@gmailcom>
2025-10-17 14:22:23 +0100

    Added User registration endpoint

--- expenses/serializer.py
+++ expenses/serializer.py
@@ -12,3 +12,18 @@ class ExpenseSerializer(serializers.ModelSerializer):
         validated_data['user'] = user
         validated_data['difference'] = validated_data.get('budget', 0) - validated_data['amount']
         return super().create(validated_data)
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user   
