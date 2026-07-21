5dee629e70514a67ade9ae934d80c6cbdcabe19d
Francis <oluremifrancisaya@gmailcom>
2026-07-21 23:00:10 +0100

    Add Book and Borrower Models

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense, Book, Borrower
 
 admin.site.register(Expense)
+admin.site.register(Book)
+admin.site.register(Borrower)
+
--- expenses/models.py
+++ expenses/models.py
@@ -29,5 +29,21 @@ class Expense(models.Model):
         return f"{self.category} - ₦{self.amount}"
     
 
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author =models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
+
+class Borrower(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrower")
+    borrow_date = models.DateField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.name
 
