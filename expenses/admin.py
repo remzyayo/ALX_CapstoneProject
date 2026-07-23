76d41762f3eeaf4e2a6fe0afc27b2488a4e9af64
Francis <oluremifrancisaya@gmailcom>
2026-07-23 08:35:14 +0100

    Priority and Task Added

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Expense, Book, Borrower, Recipe, Category
+from .models import Expense, Book, Borrower, Recipe, Category, Project, Task
 
 admin.site.register(Expense)
 admin.site.register(Book)
 admin.site.register(Borrower)
 admin.site.register(Recipe)
 admin.site.register(Category)
+admin.site.register(Project)
+admin.site.register(Task)
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title","author", "available")
@@ -19,3 +21,11 @@ class RecipeAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "cook_time", "instructions")
     list_filter = ("category")
     search_fields = ("title", "ingredients")
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title",) 
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "project", "priority", "status", "deadline",)
+    list_filter = ("priority", "status", "project",)
+    search_fields = ("title", "Project__title",)
--- expenses/models.py
+++ expenses/models.py
@@ -67,3 +67,22 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [("Low", "Low"), ("Medium", "Medium"), ("High", "High")]
+    STATUS_CHOICES = [("Not Started", "Not Started"), ("In Progress", "In Progress"), ("Done", "Done")]
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+    title = models.CharField(max_length=100)
+    deadline = models.DateField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default="Medium")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Not Started")
+
+    def __str__(self):
+        return self.title
