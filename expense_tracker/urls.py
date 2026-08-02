9f9c4d49270da21454e16ea864e3a4756c1bf54d
Francis <oluremifrancisaya@gmailcom>
2026-08-02 23:11:27 +0100

    Add Requirements

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include('expenses.url')),
+    path('', include('expenses.urls')),
     path('api/', include('expenses.urls')),
     path('api/auth/', include('rest_framework.urls')),
     path('api/register/', RegisterView.as_view(), name ='register'),
