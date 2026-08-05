9ff1f8745b6a1590457faf14ea1d39f10e85bb69
Francis <oluremifrancisaya@gmailcom>
2026-08-05 10:09:26 +0100

    Updated forms.py

--- expenses/forms.py
+++ expenses/forms.py
@@ -21,14 +21,36 @@ class ProjectForm(forms.ModelForm):
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    country = CountryField().formfield(widget=CountrySelectWidget())
-    phone_number = PhoneNumberField(required=True, help_text="Enter your phone number with country code.")
+
+    country = CountryField().formfield(
+        widget=CountrySelectWidget()
+    )
+
+    phone_number = PhoneNumberField(
+        required=True,
+        help_text="Enter your phone number with country code."
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(),
+        help_text="""
+<ul>
+<li>Your password can't be too similar to your personal information.</li>
+<li>Your password must contain at least 8 characters.</li>
+<li>Your password can't be a commonly used password.</li>
+<li>Your password can't be entirely numeric.</li>
+</ul>
+"""
+    )
+
     class Meta:
         model = User
         fields = [
             "username",
             "country",
             "phone_number",
+            "email",
             "password1",
             "password2",
+           
         ]
